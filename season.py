b0540f3957ef4d1d9aa38eaa61179950d365e387
travelwoman <107104024+travelwoman@users.noreply.github.com>
2022-06-08 13:35:12 +0530

    Update season.py

--- season.py
+++ season.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import pandas as pd
 import json
-def seasonbasedrcm(season):
+def seasonbasedrcm(season="summer"):
         
     url="https://traveltriangle.com/blog/best-places-to-visit-in-india-in-"+season+"/"
     url1="https://traveltriangle.com/blog/places-to-visit-in-"+season+"-in-india/"
@@ -13,22 +13,23 @@ def seasonbasedrcm(season):
         req = Request(url1, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
     soup=BeautifulSoup(webpage,'html.parser')
-    print(soup.get_text())
-    
+    #print(soup.get_text())
+    y=[]
     k=list(i.get_text() for i in soup.find_all("h3"))
-    for j in k[:-2]:
-        print(j)
-    c=int(input("Enter choice \n"))
+    #for j in k[:-2]:
+        #print(j)
+    #c=int(input("Enter choice \n"))
+    c=5
     if c==4:
         c=5
     o=k[c-1]
-    print(o)
+    y.append(o)
     #print(soup)
     b=soup.find(text=o).parent
     #q=b.find_next_sibling("p")
     q=b.find_next_siblings()
     x=0
-    y=[]
+    
     for i in q:
         if i.get_text().lower()=="image source":
             continue
@@ -40,4 +41,3 @@ def seasonbasedrcm(season):
         else:
             y.append(i.get_text())
     return json.dumps(y)
-
